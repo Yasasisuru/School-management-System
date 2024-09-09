c7ead361b9fded14b77053a1e1aee866099745e2
Yasas_Isuru <96908186+Yasasisuru@users.noreply.github.com>
2024-09-09 22:41:44 +0530

    create students controller file

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -10,18 +10,18 @@
 </head>
 <style>
        .sidebar {
-      height: 100vh;
-      background:rgb(201, 230, 241);
+      height: 87vh;
+
     }
     .bg-col{
-        background:rgb(201, 230, 241);
+        background:rgb(185, 255, 239);
     }
 </style>
 
 <body>
-    <div class="container">
+    <div class="">
         <div class="row">
-            <div class="col-md-12">
+            <div class="col-12 justify-content-center text-center d-flex bg-col">
                 <nav class="navbar navbar-expand-lg navbar-light">
                     <a class="navbar-brand " href="#">
                         <h2>Student Management System.</h2>
@@ -35,16 +35,18 @@
             </div>
         </div>
         <div class="row">
-            <div class=" border-right col-md-3 sidebar ">
-                <div class="list-group list-group-flush mt-5 ">
-                    <a href="#" class="list-group-item list-group-item-action bg-col">Home</a>
-                    <a href="#" class="list-group-item list-group-item-action bg-col">About</a>
-                    <a href="#" class="list-group-item list-group-item-action bg-col">Services</a>
-                    <a href="#" class="list-group-item list-group-item-action bg-col">Contact</a>
+            <div class=" border-right col-md-3 sidebar bg-col">
+                <div class="list-group list-group-flush mt-5 text-center p-2">
+                    <a href="#" class="list-group-item list-group-item-action bg-col ">Home</a>
+                    <a href="#" class="list-group-item list-group-item-action bg-col ">Student</a>
+                    <a href="#" class="list-group-item list-group-item-action bg-col ">Teacher</a>
+                    <a href="#" class="list-group-item list-group-item-action bg-col ">Courses</a>
+                    <a href="#" class="list-group-item list-group-item-action bg-col ">Enrollment</a>
+                    <a href="#" class="list-group-item list-group-item-action bg-col ">payments</a>
                 </div>
             </div>
-            <div class="col-md-9">
-                content
+            <div class="col-md-9 ">
+                 @yield('content')
             </div>
         </div>
     </div>
